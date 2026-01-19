a9c8721fad6b4b133a5976dea1332d82360a2252
Chris <chris@sicmedia.net>
2026-01-19 15:50:56 -0600

    Bump version to 1.0.32

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -68,11 +68,11 @@ android {
 
         // versionCode: Integer version for Google Play (auto-increment for each release)
         // Users never see this, but must increase with each update
-        versionCode = 26
+        versionCode = 32
 
         // versionName: User-visible version string
         // Follows semantic versioning (major.minor.patch)
-        versionName = "1.0.26"
+        versionName = "1.0.32"
 
         // TODO: Add testInstrumentationRunner for UI tests
         // testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
