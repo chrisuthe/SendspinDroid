4e0380284dfd8ce074498576165b0d18f74603ed
Chris <chris@sicmedia.net>
2026-02-14 13:28:54 -0600

    Update Gradle to 9.0.1, add wiki link, clean up gitignore
    - Bump AGP from 9.0.0 to 9.0.1
    - Add gradle-daemon-jvm.properties (new in Gradle 9, pins JDK 21)
    - Add wiki link to README Getting Started section
    - Gitignore root .idea/, build_output.txt, and Windows nul artifacts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,7 +4,7 @@
 plugins {
     // Android Gradle Plugin (AGP) - manages Android build process
     // Version 8.13.2 is current stable (Dec 2025), supports API 36
-    id("com.android.application") version "9.0.0" apply false
+    id("com.android.application") version "9.0.1" apply false
 
     // Kotlin Android plugin - enables Kotlin compilation for Android
     // Version 2.1.0 is compatible with AGP 8.13.x
