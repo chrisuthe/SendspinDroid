c58a4b1b681c828ab767191d5c2273dfd6d26e38
Chris <chris@sicmedia.net>
2026-02-18 21:52:17 -0600

    Bump version to 2.0.0-Beta2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -77,7 +77,7 @@ android {
 
         // versionName: User-visible version string
         // Follows semantic versioning (major.minor.patch[-prerelease])
-        versionName = "2.0.0-Beta1"
+        versionName = "2.0.0-Beta2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
